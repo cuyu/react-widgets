70d827b1866ec3b255673116423521f43cc11ed8
cuyu <icyarm@qq.com>
2017-05-10 17:27:53 +0800

    Fetch some other data after the first request

--- demo-async-operation/src/container.js
+++ demo-async-operation/src/container.js
@@ -2,6 +2,7 @@
  * Created by cuyu on 5/10/17.
  */
 import {connect} from 'react-redux'
+import axios from 'axios'
 import App from './App'
 
 
@@ -13,8 +14,20 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         handlePromise: () => {
             ownProps.promise.then(
-                value => dispatch({type: 'UPDATE_DATA', data: value.data}),
-                error => dispatch({type: 'UPDATE_ERROR', error: error})
+                value => {
+                    axios.get('https://api.github.com/search/repositories?q=python&sort=stars').then(
+                        value => {
+                            dispatch({type: 'UPDATE_DATA', data: value.data});
+                        },
+                        error => {
+                            dispatch({type: 'UPDATE_ERROR', error: error})
+                        }
+                    );
+                    dispatch({type: 'UPDATE_DATA', data: value.data})
+                },
+                error => {
+                    dispatch({type: 'UPDATE_ERROR', error: error})
+                }
             );
         },
     }
