5d3826d10148ae27e8d9cc8e8eefc7b630cf2935
cuyu <icyarm@qq.com>
2017-05-10 17:54:36 +0800

    Refine the Promise usage

--- demo-async-operation/src/container.js
+++ demo-async-operation/src/container.js
@@ -15,23 +15,22 @@ const mapDispatchToProps = (dispatch, ownProps) => {
         handlePromise: () => {
             ownProps.promise.then(
                 value => {
-                    axios.get('https://api.github.com/search/repositories?q=python&sort=stars').then(
-                        value => {
-                            dispatch({type: 'UPDATE_DATA', data: value.data});
-                        },
-                        error => {
-                            dispatch({type: 'UPDATE_ERROR', error: error})
-                        }
-                    );
-                    dispatch({type: 'UPDATE_DATA', data: value.data})
+                    dispatch({type: 'UPDATE_DATA', data: value.data});
+                    return axios.get('https://api.github.com/search/repositories?q=python&sort=stars');
                 },
                 error => {
-                    dispatch({type: 'UPDATE_ERROR', error: error})
+                    dispatch({type: 'UPDATE_ERROR', error: error});
+                }
+            ).then(
+                value => {
+                    dispatch({type: 'UPDATE_DATA', data: value.data});
+                },
+                error => {
+                    dispatch({type: 'UPDATE_ERROR', error: error});
                 }
             );
         },
     }
-
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
